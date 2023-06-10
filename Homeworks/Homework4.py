c1095a702de8b45153bbe00d8a6d9703f801be07
Ваше имя < ->
2023-06-10 16:35:44 +0300

    Homework4_edited

--- Homeworks/Homework4.py
+++ Homeworks/Homework4.py
@@ -3,29 +3,29 @@
 # Пользователь вводит 2 числа. n — кол-во элементов первого множества. m — кол-во элементов второго множества. 
 # Затем пользователь вводит сами элементы множеств.
 
-# def quick_sort(array):
-#     if len(array) < 1:
-#         return array
-#     else:
-#         pivot = array[0]
+def quick_sort(array):
+    if len(array) < 1:
+        return array
+    else:
+        pivot = array[0]
         
-#     less = [i for i in array[1:] if i <= pivot]
-#     greater = [i for i in array[1:] if i > pivot]
-#     return quick_sort(less) + [pivot] + quick_sort(greater)
+    less = [i for i in array[1:] if i <= pivot]
+    greater = [i for i in array[1:] if i > pivot]
+    return quick_sort(less) + [pivot] + quick_sort(greater)
 
-# n = int(input('Введите количество элементов первого множества: '))
-# m = int(input('Введите количество элементов второго множества: '))
-# list1 = [0]*n
-# list2 = [0]*m
-# for i in range(n):
-#     list1[i] = input('Введите значение элемента для первого множества:')
-# for j in range(m):
-#     list2[j] = input('Введите значение элемента для второго множества:')
+q1 = int(input('Введите количество элементов первого множества: '))
+q2 = int(input('Введите количество элементов второго множества: '))
+list1 = [0]*q1
+list2 = [0]*q2
+for i in range(q1):
+    list1[i] = int(input('Введите значение элемента для первого множества:'))
+for j in range(q2):
+    list2[j] = int(input('Введите значение элемента для второго множества:'))
 
-# common_list = list1+list2
-# quick_sort(common_list)
-# final_common_list=set(common_list)
-# print(final_common_list)
+common_list = list1 + list2
+quick_sort(common_list)
+final_common_list=set(common_list)
+print(final_common_list)
     
 
 # Задача 24: В фермерском хозяйстве в Карелии выращивают чернику. 
@@ -39,22 +39,22 @@
 # Напишите программу для нахождения максимального числа ягод, которое может собрать 
 # за один заход собирающий модуль, находясь перед некоторым кустом заданной во входном файле грядки.
 
-N = int(input("Введите количество кустов: "))
-listOfBerries = [0] * N
-for i in range(len(listOfBerries)):
-    listOfBerries[i] = int(input(f"Введите количество ягод на {i+1} кусте: "))
+# N = int(input("Введите количество кустов: "))
+# listOfBerries = [0] * N
+# for i in range(len(listOfBerries)):
+#     listOfBerries[i] = int(input(f"Введите количество ягод на {i+1} кусте: "))
     
-if len(listOfBerries) <=3:
-    maxBerries=sum(listOfBerries)
-else:
-    for i in range(len(listOfBerries)-3):
-        first = listOfBerries[i]
-        second = listOfBerries[i+1]
-        third = listOfBerries[i+2]
-        fourth = listOfBerries[i+3]
-        maxBerries = first + second + third
-        berriesToCompare = second + third + fourth
-        if berriesToCompare > maxBerries:
-            maxBerries = berriesToCompare
+# if len(listOfBerries) <=3:
+#     maxBerries=sum(listOfBerries)
+# else:
+#     for i in range(len(listOfBerries)-3):
+#         first = listOfBerries[i]
+#         second = listOfBerries[i+1]
+#         third = listOfBerries[i+2]
+#         fourth = listOfBerries[i+3]
+#         maxBerries = first + second + third
+#         berriesToCompare = second + third + fourth
+#         if berriesToCompare > maxBerries:
+#             maxBerries = berriesToCompare
             
-print(maxBerries)
+# print(maxBerries)
